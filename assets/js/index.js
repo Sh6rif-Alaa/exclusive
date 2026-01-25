87ea1064f0cd48ad08e08b58120dd849f20eb775
PhantomNoGameNoLife <phantomnogamenolife@gmail.com>
2026-01-25 18:36:54 +0200

    feat/navbar-EXC-6 adding navbar

---
+++ assets/js/index.js
@@ -0,0 +1,19 @@
+/* ----------- tailwind dark mode ------------------ */
+
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.documentElement.classList.add('dark');
+} else {
+    document.documentElement.classList.remove('dark');
+}
+
+const toggleBtn = document.getElementById('theme-toggle');
+
+toggleBtn.addEventListener('click', () => {
+    if (document.documentElement.classList.contains('dark')) {
+        document.documentElement.classList.remove('dark');
+        localStorage.theme = 'light';
+    } else {
+        document.documentElement.classList.add('dark');
+        localStorage.theme = 'dark';
+    }
+});
